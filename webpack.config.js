ff5e4edf7a69b2de9e82fb729d6b7be5ec53d589
Thanh Tran <trongthanh@gmail.com>
2017-05-15 17:07:13 +0700

    Add firebase deploy config and reconfigure build script; Update product image paths to use own hosting (to avoid image being deleted in future)

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,8 @@ module.exports = {
 	entry: './src/main.js',
 	output: {
 		path: path.resolve(__dirname, './dist'),
-		publicPath: '/dist/',
-		filename: 'build.js',
+		publicPath: '/',
+		filename: 'bundle.js',
 	},
 	module: {
 		rules: [
